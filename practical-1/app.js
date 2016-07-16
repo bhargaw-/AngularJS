99196ae93554a5b4c9fc734a98fdfd780e491000
bhargaw- <bhargaw@hotmail.co.in>
2016-07-16 12:12:34 +0530

    Practice application-1

---
+++ practical-1/app.js
@@ -0,0 +1,20 @@
+
+
+// creating module
+var myApp = angular.module('myApp', []);
+
+
+// creating controller
+var myController = function($scope){
+	$scope.message = "Welcome to angular js";
+};
+
+// registring controller with module
+myApp.controller('myController',myController);
+
+
+/*
+// creation of module, cotroller and registring controller with module in singel statement
+angular.module('myApp', []).controller('myController',function($scope){
+	$scope.message = "Welcome to angular js.";
+});*/
