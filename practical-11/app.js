21d1ac6e880769437813382a8b5423ecbd61c946
bhargaw- <bhargaw@hotmail.co.in>
2016-07-17 22:30:06 +0530

    sort rows by table header

---
+++ practical-11/app.js
@@ -0,0 +1,63 @@
+angular.module('myApp',[]).
+	controller('myController', function($scope){
+		
+		var employees = [
+
+			{
+				name :"ABCDE",
+				department:"DEPT-1",
+				gender : "Male",
+				salary : 1000,
+				date : new Date('January 01,1999')
+
+			},
+			{
+				name : "DEABC",
+				department:"DEPT-2",
+				gender : "Female",
+				salary : 4000,
+				date : new Date('February 29,2004')
+
+			},
+			{
+				name : "EABCD",
+				department:"DEPT-3",
+				gender : "Male",
+				salary : 12000,
+				date : new Date('March 15,1986')
+
+			},
+			{
+				name : "BCDEA",
+				department:"DEPT-1",
+				gender : "Female",
+				salary : 1500,
+				date : new Date('January 29,2004')
+
+			},
+			{
+				name : "CDEAB",
+				department:"DEPT-2",
+				gender : "Male",
+				salary : 9000,
+				date : new Date('February 29,2004')
+
+			}
+		];
+
+	$scope.employees = employees;
+	$scope.columnName = 'name';
+	$scope.reverseOrder = false;
+	
+	$scope.sortColumn = function(columnName){
+		$scope.columnName = columnName;
+		$scope.reverseOrder = !$scope.reverseOrder;
+	}
+
+	if($scope.reverseOrder){
+		$scope.cssClass ='arrowUp';
+	}else{
+		$scope.cssClass ='arrowDown';
+	}
+
+});
