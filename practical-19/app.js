1dff5cdecb7b753a559ab5974d31ec4fdcba17ec
bhargaw- <bhargaw@hotmail.co.in>
2016-07-24 02:07:35 +0530

    $http example -1

--- practical-19/app.js
+++ practical-19/app.js
@@ -1,7 +1,22 @@
 angular.module('myApp',[]).
-	controller('myController', function($scope,$http){
+	controller('myController', function($scope,$http,$log){
 
-		$http.get('./employee.json').then(function(response){
+		/*$http.get('./employee.json').then(function(response){
 			$scope.employees = response.data;
-		});			
+		});	*/	
+
+		$http({
+			mehotd : 'GET',
+			url : './employee.json'
+		}).then(function(response){
+			$log.info(response);
+			$log.info(response.status);
+			$log.info(response.statusText);
+			$log.info(response.config);
+			$log.info(response.config.headers.Accept);
+			$scope.employees = response.data;
+		},function(reason){
+			$log.info(reason);
+			$scope.error = reason.data;
+		});	
 });
